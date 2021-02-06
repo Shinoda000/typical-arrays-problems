c4f53d16202fe767624572496ec8289a753e1a7c
Shinoda <d1maofficial666@gmail.com>
2021-02-07 00:02:30 +0700

    feat:add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) !== true || array.length == 0) {
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) !== true || array.length == 0) {
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (Array.isArray(array) !== true || array.length == 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  }
 }
